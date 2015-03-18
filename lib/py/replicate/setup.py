880eb0cbeeffdd40222a7064d7f316dcdec72cd9
Ryan Abrams <rdabrams@gmail.com>
2015-03-18 16:53:11 -0700

    [setup] Ignore file, readme, requirements, and packaging

---
+++ lib/py/replicate/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='replicate',
+      version='0.1',
+      description='Easy replication, persistence, and network transport of objects',
+      author='Ryan Abrams',
+      author_email='rdabrams@gmail.com',
+      url='https://github.com/caervs/replicate',
+      packages=['replicate', 'replicate.tests'],
+)
