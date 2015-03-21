8471db66dd9d4a691a7e4eda65696a5cd3853e11
Ryan Abrams <rdabrams@gmail.com>
2015-03-21 11:22:51 -0700

    [setup] Add packaging logic

---
+++ lib/py/functionals/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='functionals',
+      version='0.1',
+      description='More Higher-order functions for python callables',
+      author='Ryan Abrams',
+      author_email='rdabrams@gmail.com',
+      url='https://github.com/caervs/functionals',
+      packages=['functionals', 'functionals.tests', 'functionals.examples'],
+)
