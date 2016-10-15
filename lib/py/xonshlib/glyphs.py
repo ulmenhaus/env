14c40c0990b77dbf84cd6b168f7dfb05db8e7a40
Ryan Abrams <rdabrams@gmail.com>
2016-10-15 09:43:16 -0700

    [dotfiles][lib] RC file and support for xonsh
    I've found xonsh to be a highly extensible but somewhat lacking shell. This
    change adds tools for personal environment management (e.g. managing of pass
    context, docker env variables) and some useful aliases (e.g. git and docker
    clean-up). I've attempted to make most things general purpose and put them in
    the support `xonshlib` python library. Anything which is specific to my
    machine's configuration has been put in the symbolically linked `xonshrc.py`.

---
+++ lib/py/xonshlib/glyphs.py
@@ -0,0 +1,10 @@
+class Animals(object):
+    WHALE = b"\xF0\x9F\x90\xB3".decode("utf-8")
+
+
+class Objects(object):
+    LOCK = b"\xF0\x9F\x94\x92".decode("utf-8")
+
+
+class Words(object):
+    PINATA = b"pi\xC3\xB1ata".decode("utf-8")
