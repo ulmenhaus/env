53fc442370cca8228e9972aa2d14505a806f90b7
Ryan Abrams <rdabrams@gmail.com>
2017-05-16 09:43:23 -0700

    [dotfiles] Tooling to manage clusters and pipelines
    * Add a new xonshrc module for docker pipeline team related work including easy
      instllation of a particular version of the service and CLI
    * Create a utility to easily define existing swarm clusters and the parameters
      for connecting to them including the new UCP environment for Docker Cloud
      staging

--- lib/py/xonshlib/utils.py
+++ lib/py/xonshlib/utils.py
@@ -140,21 +140,42 @@ class PasswordManager(object):
             os.remove(f.name)
 
 
-def docker_machine_env(args, stdin=None):
-    name, = args
-    if name == '-':
-        for key in list(ENV):
-            if key.startswith("DOCKER_"):
-                del ENV[key]
-        return
+class DockerClusterManager(object):
+    def __init__(self, clusters):
+        # TODO validate clusters
+        self.clusters = clusters
 
-    output = subprocess.check_output(['docker-machine', 'env', name])
-    for line in output.decode('utf-8').split("\n"):
-        if line.startswith("export "):
-            cmd = line[len("export "):]
-            key, value = cmd.split("=")
-            ENV[key] = json.loads(value)
-    ENV["DOCKER_MACHINE"] = name
+    def get_cluster_env(self, args, stdin=None):
+        name, = args
+        if name not in self.clusters:
+            self.docker_machine_env(name)
+        else:
+            params = self.clusters[name]
+            ENV["DOCKER_HOST"] = params['host']
+            if 'certs' in params:
+                ENV["DOCKER_CERT_PATH"] = params['certs']
+                ENV["DOCKER_TLS_VERIFY"] = "1"
+            else:
+                if "DOCKER_CERT_PATH" in ENV:
+                    del ENV["DOCKER_CERT_PATH"]
+                if "DOCKER_TLS_VERIFY" in ENV:
+                    del ENV["DOCKER_TLS_VERIFY"]
+        ENV["DOCKER_MACHINE"] = name
+
+    @staticmethod
+    def docker_machine_env(name):
+        if name == '-':
+            for key in list(ENV):
+                if key.startswith("DOCKER_"):
+                    del ENV[key]
+            return
+
+        output = subprocess.check_output(['docker-machine', 'env', name])
+        for line in output.decode('utf-8').split("\n"):
+            if line.startswith("export "):
+                cmd = line[len("export "):]
+                key, value = cmd.split("=")
+                ENV[key] = json.loads(value)
 
 
 def command_for_container_config(config):
