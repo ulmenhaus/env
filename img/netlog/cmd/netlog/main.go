033a29d82c503b97c2e1ae412cdc6fdd1abfca72
Ryan Abrams <rdabrams@gmail.com>
2018-06-17 13:05:44 -0700

    WIP working frame capture

--- img/netlog/cmd/netlog/main.go
+++ img/netlog/cmd/netlog/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
+	"os/exec"
 
 	"github.com/jroimartin/gocui"
+	"github.com/ulmenhaus/env/img/netlog/parse"
 )
 
 type SelectableList struct {
@@ -49,21 +51,18 @@ func (sl *SelectableList) Layout(g *gocui.Gui) error {
 	return nil
 }
 
+func (s1 *SelectableList) Add(item string) {
+	// TODO thread safe
+	s1.items = append(s1.items, item)
+}
+
 func main() {
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
-		log.Panicln(err)
+		panic(err)
 	}
 	sl := &SelectableList{
-		items: []string{
-			"item0",
-			"item1",
-			"item2",
-			"item3",
-			"item4",
-			"item5",
-			"item6",
-		},
+		items: []string{},
 	}
 
 	defer g.Close()
@@ -71,19 +70,48 @@ func main() {
 	g.SetManagerFunc(sl.Layout)
 
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
+		panic(err)
 	}
 
 	if err := g.SetKeybinding("", gocui.KeyArrowDown, gocui.ModNone, sl.Down); err != nil {
-		log.Panicln(err)
+		panic(err)
 	}
 
 	if err := g.SetKeybinding("", gocui.KeyArrowUp, gocui.ModNone, sl.Up); err != nil {
-		log.Panicln(err)
+		panic(err)
 	}
 
+	// TODO pass args from cli
+	go func() {
+		// XXX For PoC only do IPv4 traffic
+		cmd := exec.Command("tcpdump", "-i", "any", "-vv", "-XX", "ip")
+		p, err := parse.NewTCPDumpParser()
+		if err != nil {
+			panic(err)
+		}
+		out, err := cmd.StdoutPipe()
+		if err != nil {
+			panic(err)
+		}
+		// TODO collect stderr
+		err = cmd.Start()
+		if err != nil {
+			panic(err)
+		}
+		eventChan, errChan := p.ParseStream(out)
+		for event := range eventChan {
+			g.Update(func(g *gocui.Gui) error {
+				sl.Add(fmt.Sprintf("frame of size: %d", len(event.Frame)))
+				return nil
+			})
+		}
+		err = <-errChan
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+	}()
 	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
+		panic(err)
 	}
 }
 
--- img/netlog/parse/tcpdump.go
+++ img/netlog/parse/tcpdump.go
@@ -1,8 +1,11 @@
 package parse
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -13,18 +16,38 @@ type EthernetEvent struct {
 	Frame models.IEEEFrame
 }
 
-func ParseTCPDumpHexDump(b []byte) ([]byte, error) {
+type TCPDumpParser struct {
+	prefixExpr *regexp.Regexp
+	dumpExpr   *regexp.Regexp
+	lenExpr    *regexp.Regexp
+}
+
+func NewTCPDumpParser() (*TCPDumpParser, error) {
+	prefixExpr, err := regexp.Compile("^[0-9][0-9]:[0-9][0-9]:[0-9][0-9].*length ")
+	if err != nil {
+		return nil, err
+	}
+	dumpExpr, err := regexp.Compile("^\t0x")
+	if err != nil {
+		return nil, err
+	}
+	return &TCPDumpParser{
+		prefixExpr: prefixExpr,
+		dumpExpr:   dumpExpr,
+	}, nil
+}
+
+func (p *TCPDumpParser) ParseHexDump(b []byte) ([]byte, error) {
 	bytes := []byte{}
+	if b[len(b)-1] == '\n' {
+		b = b[:len(b)-1]
+	}
 	lines := strings.Split(string(b), "\n")
 	for _, line := range lines {
-		if len(line) < 56 {
-			continue
+		if len(line) < 50 {
+			return nil, fmt.Errorf("invalid hex dump line -- fewer than 50 chars")
 		}
-		parts := strings.Split(strings.Replace(line[:56], " ", "", -1), ":")
-		if len(parts) < 2 {
-			return nil, fmt.Errorf("invalid dump -- no ':'")
-		}
-		chars := parts[1]
+		chars := strings.Replace(line[10:50], " ", "", -1)
 		if (len(chars) % 2) != 0 {
 			return nil, fmt.Errorf("invalid dump -- not an even number of chars")
 		}
@@ -39,6 +62,78 @@ func ParseTCPDumpHexDump(b []byte) ([]byte, error) {
 	return bytes, nil
 }
 
-func ParseTCPDumpStream(r io.Reader) (<-chan EthernetEvent, <-chan error) {
+func (p *TCPDumpParser) parseStream(r io.Reader, eventChan chan<- EthernetEvent, errChan chan<- error) {
+	b := bufio.NewReader(r)
+	current := []byte{}
+	var err error
+	defer func() {
+		close(eventChan)
+		errChan <- err
+		close(errChan)
+	}()
+	for {
+		current, err = b.ReadBytes('\n')
+		if err != nil {
+			return
+		}
+		if p.prefixExpr.Match(current) {
+			break
+		}
+	}
+	for {
+		length := 0
+		parts := bytes.Split(current, []byte("length "))
+		if len(parts) != 2 {
+			err = fmt.Errorf("Could not parse prefix line. Had %d parts.", len(parts))
+			err = fmt.Errorf("%s %s", err, current)
+			return
+		}
+		for _, digit := range parts[1] {
+			if '0' <= digit && digit <= '9' {
+				length = (length * 10) + int(digit-'0')
+			} else {
+				break
+			}
+		}
+		// XXX does not handle 802.1Q headers
+		expected := (length / 16) + 1
+		if (length % 16) != 0 {
+			expected += 1
+		}
+		hexDump := []byte{}
+		actual := 0
+		for {
+			current, err = b.ReadBytes('\n')
+			if p.dumpExpr.Match(current) {
+				actual += 1
+				hexDump = append(hexDump, current...)
+			}
+			if actual == expected {
+				var parsed []byte
+				parsed, err = p.ParseHexDump(hexDump)
+				if err != nil {
+					return
+				}
+				eventChan <- EthernetEvent{
+					Frame: models.IEEEFrame(parsed),
+				}
+				break
+			}
+		}
+		current, err = b.ReadBytes('\n')
+		if err != nil {
+			return
+		}
+		if !p.prefixExpr.Match(current) {
+			err = fmt.Errorf("expected prefix line after dump")
+			return
+		}
+	}
+}
 
+func (p *TCPDumpParser) ParseStream(r io.Reader) (<-chan EthernetEvent, <-chan error) {
+	eventChan := make(chan EthernetEvent)
+	errChan := make(chan error)
+	go p.parseStream(r, eventChan, errChan)
+	return eventChan, errChan
 }
