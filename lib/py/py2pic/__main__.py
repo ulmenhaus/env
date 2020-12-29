e8904cc9016fdaca7058cd79e784f5e9570607dc
Ryan Abrams <rdabrams@gmail.com>
2020-12-29 12:13:32 -0800

    [py2pic] Support rendering legacy .m4 diagrams as well

--- lib/py/py2pic/__main__.py
+++ lib/py/py2pic/__main__.py
@@ -10,50 +10,64 @@ WRAPPER_TEMPLATE = """
 \\documentclass<article>
 \\pagestyle<empty>
 \\usepackage<pstricks>
+\\usepackage<amsmath>
 \\begin<document>
 \\input<{wrapped}.tex>
 \\end<document>
 """
 
 
-def main():
-    d = Drawing()
-    draw_file = sys.argv[1]
-    if not draw_file.endswith(".py"):
-        draw_file += ".py"
-    base = os.path.basename(draw_file)
-    nopy = ".".join(base.split(".")[:-1])
+def _convert_to_m4(draw_file):
     global draw
     with open(draw_file) as f:
         exec(f.read(), globals())
+    d = Drawing()
     draw(d)
+    return d.render()
+
+
+def main():
+    draw_file = sys.argv[1]
+    name = os.path.basename(draw_file)
+    if draw_file.endswith(".py"):
+        name = name[:-3]
+        m4 = _convert_to_m4(draw_file)
+    elif draw_file.endswith(".m4"):
+        name = name[:-3]
+        with open(draw_file) as f:
+            m4 = f.read()
+    else:
+        raise ValueError("I do not know how to render {}".format(name))
+
     if not os.path.exists("build"):
         os.mkdir("build")
         for fpath in glob.glob(os.path.join(os.path.dirname(__file__),
                                             "*.m4")):
-            shutil.copyfile(fpath, os.path.join("build", os.path.basename(fpath)))
+            shutil.copyfile(fpath,
+                            os.path.join("build", os.path.basename(fpath)))
 
     os.chdir("build")
 
-    with open("{}.m4".format(nopy), 'w') as f:
-        f.write(d.render())
-    with open("{}_wrapper.tex".format(nopy), 'w') as f:
+    with open("{}.m4".format(name), 'w') as f:
+        f.write(m4)
+
+    with open("{}_wrapper.tex".format(name), 'w') as f:
         f.write(
-            WRAPPER_TEMPLATE.format(wrapped=nopy).replace("<", "{").replace(
+            WRAPPER_TEMPLATE.format(wrapped=name).replace("<", "{").replace(
                 ">", "}"))
 
     cmds = [
-        "rm -f {nopy}_wrapper.eps",
-        "m4 pstricks.m4 {nopy}.m4 > {nopy}.pic",
-        "dpic -p {nopy}.pic > {nopy}.tex",
-        "latex {nopy}_wrapper.tex",
-        "dvips {nopy}_wrapper.dvi",
-        "ps2eps {nopy}_wrapper.ps",
-        "epspdf {nopy}_wrapper.eps",
-        "pdf2svg {nopy}_wrapper.pdf {nopy}.svg",
+        "rm -f {name}_wrapper.eps",
+        "m4 pstricks.m4 {name}.m4 > {name}.pic",
+        "dpic -p {name}.pic > {name}.tex",
+        "latex {name}_wrapper.tex",
+        "dvips {name}_wrapper.dvi",
+        "ps2eps {name}_wrapper.ps",
+        "epspdf {name}_wrapper.eps",
+        "pdf2svg {name}_wrapper.pdf {name}.svg",
     ]
     for cmd in cmds:
-        subprocess.check_call(["bash", "-c", cmd.format(nopy=nopy)])
+        subprocess.check_call(["bash", "-c", cmd.format(name=name)])
 
 
 if __name__ == "__main__":
