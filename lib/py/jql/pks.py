35883879c5efa15b7bb1ea0682a0a2549da1dc7a
Ryan Abrams <rdabrams@gmail.com>
2021-08-29 12:43:18 -0700

    Add a macro for automatic attention cycle generation
    Part of the timedb schema, this will allow you to select an item you would like
    to be in your attention for a given cycle (quarter) and it will auto-create an
    attention task for it. The macro goes further to group related tasks under
    meaningful ancestors that finds a balance between decluttering and not
    introducing too many levels of indirection.
    
    Create a jql python module to contain the newly common function for generating task pks

---
+++ lib/py/jql/pks.py
@@ -0,0 +1,46 @@
+import datetime
+
+
+def pk_for_task(task, parent):
+    action = task['Action']
+    preposition = "with"
+    prepreposition = ""
+    if action in ("Attend", "Migrate", "Transfer"):
+        preposition = "to"
+    elif action in ("Present", ):
+        preposition = "on"
+    elif action in ("Buy", ):
+        preposition = "for"
+    elif action in (
+            "Ideate",
+            "Deliberate",
+    ):
+        preposition = "on"
+    elif action in ("Vacation", ):
+        prepreposition = "in"
+    elif action in ("Lunch", "Dine", "Shop"):
+        prepreposition = "at"
+    direct_clause, indirect_clause = "", ""
+    if task['Direct']:
+        direct_clause = " {}".format(task['Direct'])
+        if prepreposition:
+            direct_clause = " {}{}".format(prepreposition, direct_clause)
+    if task['Indirect']:
+        indirect_clause = " {} {}".format(preposition, task['Indirect'])
+    mandate = "{}{}{}".format(action, direct_clause, indirect_clause)
+    if task["Parameters"]:
+        marker = " at" if action in ("Extend", "Improve", "Sustain") else ","
+        mandate += "{} {}".format(marker, task['Parameters'])
+    planned_start, planned_span = task["Param~Start"], task["Param~Span"]
+    if parent.get("Indirect") == "breakdown":
+        planned_start, planned_span = parent["Param~Start"], parent[
+            "Param~Span"]
+    distinguisher = (
+        datetime.datetime(1969, 12, 31) +
+        datetime.timedelta(days=int(planned_start))).strftime("%d %b %Y")
+    # TODO need to use parent span here but will do once we can edit the table
+    # to match
+    if task["Param~Span"] and task["Param~Span"] != "Day":
+        distinguisher = "{} of {}".format(task["Param~Span"], distinguisher)
+    mandate += " ({})".format(distinguisher)
+    return mandate
