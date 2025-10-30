b8067c7e0231eb8622dd2926d2edf19cb3190447
Ryan Abrams <rdabrams@gmail.com>
2025-10-30 08:14:35 -0700

    Move diagram rendering to support new pymdown approach

--- lib/py/timedb/markdown/render.py
+++ lib/py/timedb/markdown/render.py
@@ -1,4 +1,6 @@
+import hashlib
 import os
+import subprocess
 import time
 
 
@@ -15,3 +17,122 @@ def watch_for_update(filepath, poll_interval=2):
             # HACK might still be writing the file so sleep for another 100ms
             time.sleep(.1)
             yield
+
+
+def render_pic(src):
+    wd = os.getcwd()
+    target = "{}.svg".format(src[:-3])
+    if not os.path.exists(target):
+        subprocess.check_call([
+            "docker",
+            "run",
+            "-i",
+            "--rm",
+            "--entrypoint=pp",
+            "-w",
+            wd,
+            "-v",
+            "{}:{}".format(os.getenv("JQL_HOST_MOUNT_PREFIX") + wd, wd),
+            "ulmenhaus/env",
+            src,
+        ])
+    img = '<div style="text-align:center; padding-bottom: 25px;"><img src="{}" /></div>\n'.format(
+        target)
+    return img
+
+
+def render_pics(grouped, meta):
+    for i, group in enumerate(grouped):
+        lines = group.split("\n")
+        if lines[0] not in ["```pic.py", "```m4", "```plt.py"]:
+            yield group
+            continue
+        group_meta = meta.get(i, "").encode("utf-8")
+        suffix = lines[0][3:]
+        body = "\n".join(lines[1:-1]).encode("utf-8")
+        sha = hashlib.sha256()
+        sha.update(body)
+        sha.update(b"\x00")
+        sha.update(group_meta)
+        dgst = sha.hexdigest()[:8]
+        src = "build/{}.{}".format(dgst, suffix)
+        meta_src = src + ".meta"
+        if not os.path.exists(src):
+            with open(src, 'wb') as f:
+                f.write(body)
+            with open(meta_src, 'wb') as f:
+                f.write(group_meta)
+        yield render_pic(src)
+
+
+def inject_externals(default_project, markdown):
+    suffix_to_language = {
+        "py": "python",
+    }
+    lines = markdown.split("\n")
+    grouped = []
+    while lines:
+        first = lines.pop(0)
+        if first.startswith("```"):
+            while lines:
+                to_add = lines.pop(0)
+                first += "\n" + to_add
+                if to_add.startswith("```"):
+                    break
+        grouped.append(first)
+
+    final = []
+    meta = {}
+    for i, group in enumerate(grouped):
+        if group.startswith("```external"):
+            lines = group.split("\n")
+            meta[i] = "\n".join(lines[1:-1])
+            args = lines[0].split(" ")[1:]
+            filename = args[0]
+            until = None
+            after = None
+            if ":" in filename:
+                filename, section = filename.split(":")
+                comment_prefix = _comment_prefix(filename)
+                after = f"{comment_prefix} start_section: {section}"
+                until = f"{comment_prefix} end_section: {section}"
+            suffix = filename.split(".", 1)[1]
+            if "/" not in filename:
+                filename = "{}/{}".format(default_project, filename)
+            filename = "snippets/{}".format(filename)
+
+            for arg in args[1:]:
+                if match := re.match('until="(?P<until>.*)"\Z', arg):
+                    until = match.group("until")
+                elif match := re.match('after="(?P<after>.*)"\Z', arg):
+                    after = match.group("after")
+            with open(filename) as f:
+                body = f.read()
+                if after:
+                    body = body.split(after, 1)[1]
+                if until:
+                    body = body.split(until, 1)[0]
+            group = "```{}\n{}\n```".format(
+                suffix_to_language.get(suffix, suffix), body)
+        final.append(group)
+
+    final = render_pics(final, meta)
+    return resolve_jql_links("\n".join(final))
+
+def resolve_jql_links(base_markdown):
+    inputs = base_markdown.split("@{nouns ")
+    parts = []
+    for i, inpt in enumerate(inputs):
+        if i == 0:
+            parts.append(inpt)
+            continue
+        # technically disallows to link with a noun that has a colon but shrug
+        try:
+            ref, rest = inpt.split(":", 1)
+        except:
+            raise Exception("Exception breaking line", inpt)
+        parts.append(_render_link(ref, "/nouns " + ref))
+        parts.append(rest)
+    return "".join(parts)
+
+
